90a0f486e0537d45982bcc79141799c86b10b313
Animesh Shrivastava <animeshshrivastava@outlook.in>
2018-12-28 18:41:37 +0530

    MVC party app introduction

---
+++ PartyInviteMVC5Solution/PartyInviteMVC5WebApp/Controllers/PartyController.cs
@@ -0,0 +1,56 @@
+﻿using PartyInviteMVC5WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInviteMVC5WebApp.Controllers
+{
+    public class PartyController : Controller
+    {
+        // GET: Party
+        public ActionResult Index()
+        {
+            int hours = DateTime.Now.Hour;
+            ViewBag.Greet = hours < 18 ? "Good AfterNoon " + DateTime.Now : "Good Evening" + DateTime.Now;
+            return View();
+
+        }
+        //TOP MOST LAYER IS ACTION  
+        public ViewResult RSVPForm()
+        {
+            return View();
+        }
+        //default is always get() method
+        [HttpPost]
+        public ViewResult RSVPForm(GuestResponse model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", model);
+            }
+            return View();
+        }//it will hold the data in the textbox and thanks view and model as a data
+
+
+        public ContentResult ContentList()
+        {
+            var result = "<div Style='background-color:cornflower;text-align:center;color:white'><h1>This is the Content Result</h1></div>";
+            return Content(result);
+        }
+
+        public JsonResult List()
+        {
+            var result = Json(new { Name = "Phantom", Email = "phantom@p.com", mobile = "123456789" });
+            return Json(result,JsonRequestBehavior.AllowGet);
+        }
+
+        public string Greeting()
+        {
+            return $"Welcome to MVC5{DateTime.Now}";
+        }
+
+        
+    }
+}
---
+++ PartyInviteMVC5Solution/PartyInviteMVC5WebApp/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInviteMVC5WebApp.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="First Name Cannot Be Empty")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name Cannot Be Empty")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Cannot Be Empty")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Mobile Cannot be Empty")]
+        public string Mobile { get; set; }
+        [Required(ErrorMessage = "Please Choose One Option")]
+        public bool? WillAttend { get; set; }
+
+
+
+    }
+}
